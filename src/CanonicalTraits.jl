34ad35d2867a2021553f671e4f9e9ccddc1aab68
thautwarm <twshere@outlook.com>
2019-09-01 22:22:10 +0800

    done

--- src/CanonicalTraits.jl
+++ src/CanonicalTraits.jl
@@ -1,5 +1,126 @@
 module CanonicalTraits
+using MLStyle
 
-greet() = print("Hello World!")
+export @trait, instance
+
+abstract type TraitParam end
+
+abstract type TraitParamCons{A, B<:TraitParam} <: TraitParam
+end
+
+abstract type TraitParamNil <: TraitParam
+end
+
+abstract type Trait{P <: TraitParam} end
+
+abstract type TraitInstance end
+
+struct FuncType{Args, Ret, F}
+end
+
+function_sig(sig) =
+    @match sig begin
+        :($arg => $ret) =>
+            let arg = function_sig(arg),
+                ret = function_sig(ret)
+                :($FuncType{$arg, $ret})
+            end
+        :[$(args...)] => :($Tuple{$(map(function_sig, args)...)})
+        Expr(hd, tl...) => Expr(hd, map(function_sig, tl)...)
+        a => a
+    end
+
+extract_tvars(var :: Union{Symbol, Expr})::Symbol =
+    @match var begin
+        :($a <: $_) => a
+        :($a >: $_) => a
+        :($_ >: $a >: $_) => a
+        :($_ <: $a <: $_) => a
+        a::Symbol         => a
+    end
+
+extract_forall!(var, coll::Vector{Any}) =
+    @match var begin
+        :($a where {$(tvars...)}) => begin
+                append!(coll, tvars)
+                extract_forall!(a, coll)
+            end
+        Expr(hd, tl...) => Expr(hd, map(x -> extract_forall!(x, coll), tl)...)
+        a => a
+    end
+
+extract_method(stmt::Union{Expr, LineNumberNode})::Union{Tuple, LineNumberNode} =
+    @match stmt begin
+        :($f :: $arg => $ret) => (f, function_sig(arg),  function_sig(ret))
+        _            => stmt
+    end
+
+function mk_method(trait :: Any, f :: Any, argsty :: Any, retty :: Any, syms :: Syms) where Syms <: AbstractArray{Symbol}
+    foralls = Any[]
+    argsty  = extract_forall!(argsty, foralls)
+    @when let :($tp{$(argtys...)}) = argsty,
+              (tp === Tuple).?
+
+        basename = gensym(string(f))
+        argnames = [Symbol(basename, i) for i in 1:length(argtys)]
+        annos    = [:($argname :: $argty) for (argname, argty) in zip(argnames, argtys)]
+        quote
+            function $f($(annos...))::$retty where {$(syms...), $(foralls...)}
+                $trait($(syms...)).$f($(argnames...))
+            end
+        end
+    @otherwise
+        error("Malformed method $f($argsty) for trait $trait.")
+    end
+end
+
+mk_trait(syms::Syms) where Syms <: AbstractArray{Symbol} =
+    @match syms begin
+    [] => TraitParamNil
+    [hd, tl...] => :($TraitParamCons{$hd, $(mk_trait(tl))})
+    end
+
+"""
+get_instance_type
+"""
+function instance(trait :: Type{<:Trait})
+end
+
+function trait(sig, block)
+    @when let :($trait_ty{$(tvars...)}) = sig,
+              Expr(:block, stmts...) = block
+
+        tsyms      = map(extract_tvars, tvars)
+        methods    = map(extract_method, stmts)
+        methods    = filter(x -> !(x isa LineNumberNode), methods)
+        interfaces = [mk_method(trait_ty, f, argsty, retty, tsyms)
+                      for (f, argsty, retty) in methods]
+        instance_   = Symbol(string(trait_ty), "#instance")
+        interface_names = map(first, methods)
+        method_types = map(x -> Symbol(x, "#t"), interface_names)
+        fields = [:($a :: $b) for (a, b) in zip(interface_names, method_types)]
+
+        quote
+            abstract type $sig <: $Trait{$(mk_trait(tsyms))}
+            end
+            struct $instance_{$(method_types...)}
+                $(fields...)
+            end
+            $(interfaces...)
+            $CanonicalTraits.instance(::Type{$trait_ty}) = $instance_
+        end
+    @otherwise
+        error("Malformed trait $sig definition.")
+    end
+end
+
+macro trait(sig, block=Expr(:block))
+    trait(sig, block) |> esc
+end
+
+(trait::Type{<:Trait})(types...) = begin
+    types = join(map(string, types), ", ")
+    error("Not implemented trait $trait for ($types).")
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,88 @@
 using CanonicalTraits
 using Test
 
+@trait Monoid{A} begin
+    mempty :: [] => A
+    (⊕)    :: [A, A] => A
+end
+
+const int_monoid = instance(Monoid)(() -> 0::Int, +)
+const vect_monoid(::Type{T}) where T = instance(Monoid)(() -> T[], vcat)
+
+Monoid(it) = default_monoid
+Monoid(::Type{Int}) = int_monoid
+Monoid(::Type{Vector{T}}) where T = vect_monoid(T)
+
+@trait ToString{A} begin # like Show in Haskell
+    to_str :: [A] => String
+end
+
+const default_to_string = instance(ToString)(Base.string)
+ToString(it) = default_to_string
+
+# using Polynomials
+# @trait Vect{V, E} begin
+#     coef       :: ([Num, V] where Num <: Number) => V
+#     vec_add    :: ([V, V]) => V
+#     scalar_add :: ([E, V]) => V
+# end
+
+# const polynomial_vect = instance(Vect)(
+#     function polynomial_coef(num::Num1, vec::Poly{Num2}) where {Num1 <: Number, Num2 <: Number}
+#         num * vec
+#     end,
+#     function polynomial_vec_add(vec1 :: Poly{Num}, vec2 :: Poly{Num}) where Num <: Number
+#         vec1 + vec2
+#     end,
+#     function polynomial_scalar_add(scalar::Num, vec::Poly{Num}) where Num <: Number
+#         scalar + vec
+#     end
+# )
+
+# Vect(::Type{Poly{Num}}, ::Type{Num}) where Num <: Number = polynomial_vect
+
+
+# @trait Dot{V} begin
+#     dot :: [V, V] => Real
+#     gram_schmidt :: [V, Set{V}] => V
+# end
+
+# const polynomial_dot = instance(Dot)(
+#     function poly_dot(v1 :: Poly{Num}, v2 :: Poly{Num})::Real where Num <: Number
+#         f = polyint(v1 * v2)
+#         f(1) - f(-1)
+#     end,
+#     function poly_gram_schmidt(v :: Poly{Num}, vs :: Set{Poly{Num}})::Poly{Num} where Num <: Number
+#         for other in vs
+#             coeff = dot(v, other)
+#         end
+#     end
+# )
+
+# Dot(::Type{Poly{<:Number}}) = polynomial_dot
+
+# 100 ⊕ 2 |> println
+# @btime 100 ⊕ 2
+# @btime 100 + 2
+# @btime vcat([1, 2, 3], [3, 4, 5])
+# @btime [1, 2, 3] ⊕ [3, 4, 5]
+# @btime 1 ⊕ 2
+
+
 @testset "CanonicalTraits.jl" begin
     # Write your own tests here.
+    @test_throws Any begin
+        "2" ⊕ "3"
+    end
+
+    @test 102 == begin
+        100 ⊕ 2
+    end
+
+    @test [1, 2, 3, 3] == begin
+        [1, 2] ⊕ [3, 3]
+    end
+
+    @test map(to_str, ["123", 10]) == map(string, ["123", 10])
+
 end
